db7c795d91008685fc1f7a263b544c6bd6368461
al-ctrl <aldyanganteng00@gmail.com>
2026-09-23 14:07:12 +0700

    fix: update lagu opening detik 1

--- src/main.jsx
+++ src/main.jsx
@@ -51,7 +51,7 @@ const startWebsite = async () => {
       audio.preload = "auto";
 
       // Mulai dari 00:18
-      audio.currentTime = 120;
+      audio.currentTime = 119;
 
       // Play dulu dengan volume 0
       audio.volume = 0;
